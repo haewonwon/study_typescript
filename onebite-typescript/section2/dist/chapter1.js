6078aab12a71de6d18c924c52ea26ed40445fc5c
haewonwon <khcw1029@daum.net>
2025-05-22 17:19:40 +0900

    Add: chapter 3 내용 추가

---
+++ onebite-typescript/section2/dist/chapter1.js
@@ -0,0 +1,26 @@
+// number
+let num1 = 136;
+let num2 = -136;
+let num3 = 0.123;
+let num4 = -0.123;
+let num5 = Infinity;
+let num6 = -Infinity;
+let num7 = NaN;
+// string
+let str1 = 'Hello';
+let str2 = 'Hello';
+let str3 = `Hello`;
+let str4 = `Hello ${num1}`;
+// boolean
+let bool1 = true;
+let bool2 = false;
+// null
+let null1 = null;
+// undefined
+let unde1 = undefined;
+// 리터럴 타입
+// 리터럴 == 값
+let numA = 10;
+let strA = 'hello';
+let boolA = false;
+export {};
