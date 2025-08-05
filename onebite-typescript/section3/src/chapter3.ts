d483109f74183b7ff557d92b12e3a2d2796a8206
haewonwon <khcw1029@daum.net>
2025-08-05 21:16:36 +0900

    Add: 객체 타입의 호환성 추가

--- onebite-typescript/section3/src/chapter3.ts
+++ onebite-typescript/section3/src/chapter3.ts
@@ -1,61 +1,83 @@
-// Unknown 타입
-function unknownExample() {
-  let a: unknown = 1;
-  let b: unknown = 'hello';
-  let c: unknown = true;
-  let d: unknown = [1, 2, 3];
-  let e: unknown = null;
-  let f: unknown = undefined;
+// 기본 타입 간의 호환성
 
-  let unknownVar: unknown;
+let num1: number = 10;
+let num2: 10 = 10;
 
-  // let num: number = unknownVar;
-  // Error: Type 'unknown' is not assignable to type 'number'.
+num1 = num2;
 
-  // let str: string = unknownVar;
-  // Error: Type 'unknown' is not assignable to type 'string'.
+// 객체 타입 간의 호환성
+// 어떤 객체 타입을 다른 객체 타입으로 취급해도 괜찮은가?
 
-  // let bool: boolean = unknownVar;
-  // Error: Type 'unknown' is not assignable to type 'boolean'.
-}
+type Animal = {
+  name: string;
+  color: string;
+};
 
-// Never 타입
-function neverExample() {
-  function neverFunc(): never {
-    while (true) {}
-  }
+type Dog = {
+  name: string;
+  color: string;
+  breed: string;
+};
 
-  let num: number = neverFunc();
-  let str: string = neverFunc();
-  let bool: boolean = neverFunc();
+let animal: Animal = {
+  name: '기린',
+  color: 'yellow',
+};
 
-  let neverVar: never;
+let dog: Dog = {
+  name: '돌돌이',
+  color: 'brown',
+  breed: '진도',
+};
 
-  // neverVar = 10;
-  // Error: Type 'number' is not assignable to type 'never'.
-  // neverVar = 'hello';
-  // Error: Type 'string' is not assignable to type 'never'.
-  // neverVar = true;
-  // Error: Type 'boolean' is not assignable to type 'never'.
-}
+animal = dog;
 
-// Void 타입
-function voidExample() {
-  function voidFunc(): void {
-    console.log('Hello, World!');
-    return undefined;
-  }
-}
+// dog = animal;
+// // Error: Type 'Animal' is not assignable to type 'Dog'.
 
-// Any 타입
-function anyExample() {
-  let unknownVar: unknown;
-  let anyVar: any;
-  let undefinedVar = undefined;
-  let neverVar: never;
+// 슈퍼 타입
+type Book = {
+  name: string;
+  price: number;
+};
 
-  anyVar = unknownVar; // OK, unknown can be assigned to any
-  undefinedVar = anyVar; // OK, any can be assigned to undefined
-  // neverVar = anyVar;
-  // Error: Type 'any' is not assignable to type 'never'.
-}
+// 서브 타입
+type ProgrammingBook = {
+  name: string;
+  price: number;
+  skill: string;
+};
+
+let book: Book;
+let programmingBook: ProgrammingBook = {
+  name: '한 입 크기로 잘라먹는 리액트',
+  price: 33000,
+  skill: 'reactjs',
+};
+
+// 1)
+book = programmingBook;
+// programmingBook = book;
+// // Error: Type 'Book' is not assignable to type 'ProgrammingBook'.
+// 다운 캐스팅임
+
+// 2) 초과 프로퍼티 검사
+let book2: Book = {
+  name: '한 입 크기로 잘라먹는 리액트',
+  price: 33000,
+  // skill: 'reactjs', // skill 속성이 Book 타입에 없으므로 할당 불가
+};
+
+// 3) 이런 경우는 허용됨
+let boo3: Book = programmingBook;
+
+// 4) 함수의 인수로 객체 리터럴을 전달할 때도 마찬가지임
+function func(book: Book) {}
+
+func({
+  name: '한 입 크기로 잘라먹는 리액트',
+  price: 33000,
+  // skill: 'reactjs',
+});
+
+func(programmingBook);
