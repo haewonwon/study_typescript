03e3367fc661e2946082693874fc095befac6ca5
haewonwon <khcw1029@daum.net>
2026-02-10 15:50:30 +0900

    Add: 상태관리와 Props(1) 내용 추가

--- onebite-typescript/section11/src/App.tsx
+++ onebite-typescript/section11/src/App.tsx
@@ -1,5 +1,38 @@
+import { useState, useRef, useEffect } from 'react';
+import Editor from '../src/components/Editor';
+
+interface Todo {
+  id: number;
+  content: string;
+}
+
 function App() {
-  return <div></div>;
+  // useState은 하나의 타입 변수를 받는 제네릭 함수임
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const idRef = useRef(0);
+
+  const onClickAdd = (text: string) => {
+    setTodos([
+      ...todos,
+      {
+        id: idRef.current++,
+        content: text,
+      },
+    ]);
+  };
+
+  useEffect(() => {
+    console.log(todos);
+  }, [todos]);
+
+  return (
+    <div>
+      <h1>Todo</h1>
+
+      <Editor onClickAdd={onClickAdd} />
+    </div>
+  );
 }
 
 export default App;
