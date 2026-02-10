0d1d6841428ca59d1dfa7f873405ccf6de2b8027
haewonwon <khcw1029@daum.net>
2026-02-10 17:01:52 +0900

    Add: 상태관리와 Props(2)

--- onebite-typescript/section11/src/App.tsx
+++ onebite-typescript/section11/src/App.tsx
@@ -1,25 +1,64 @@
-import { useState, useRef, useEffect } from 'react';
-import Editor from '../src/components/Editor';
+import { useState, useRef, useEffect, useReducer } from 'react';
+import Editor from './components/Editor';
+import TodoItem from './components/TodoItem';
+import { type Todo } from './types';
 
-interface Todo {
-  id: number;
-  content: string;
+type Action =
+  | {
+      type: 'CREATE';
+      data: {
+        id: number;
+        content: string;
+      };
+    }
+  | {
+      type: 'DELETE';
+      id: number;
+    };
+
+function reducer(state: Todo[], action: Action) {
+  switch (action.type) {
+    case 'CREATE': {
+      return [...state, action.data];
+    }
+    case 'DELETE': {
+      return state.filter((it) => it.id !== action.id);
+    }
+  }
 }
 
 function App() {
   // useState은 하나의 타입 변수를 받는 제네릭 함수임
-  const [todos, setTodos] = useState<Todo[]>([]);
+  // const [todos, setTodos] = useState<Todo[]>([]);
+  const [todos, dispatch] = useReducer(reducer, []);
 
   const idRef = useRef(0);
 
   const onClickAdd = (text: string) => {
-    setTodos([
-      ...todos,
-      {
+    // setTodos([
+    //   ...todos,
+    //   {
+    //     id: idRef.current++,
+    //     content: text,
+    //   },
+    // ]);
+
+    dispatch({
+      type: 'CREATE',
+      data: {
         id: idRef.current++,
         content: text,
       },
-    ]);
+    });
+  };
+
+  const onClickDelete = (id: number) => {
+    // setTodos(todos.filter((todo) => todo.id !== id));
+
+    dispatch({
+      type: 'DELETE',
+      id: id,
+    });
   };
 
   useEffect(() => {
@@ -29,8 +68,12 @@ function App() {
   return (
     <div>
       <h1>Todo</h1>
-
       <Editor onClickAdd={onClickAdd} />
+      <div>
+        {todos.map((todo) => (
+          <TodoItem key={todo.id} {...todo} onClickDelete={onClickDelete} />
+        ))}
+      </div>
     </div>
   );
 }
